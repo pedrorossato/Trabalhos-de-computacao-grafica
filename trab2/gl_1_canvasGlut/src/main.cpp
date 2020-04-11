06be32b88261ad63177928bff1210ed2a4d85a04
pedrorossato <phrossato@inf.ufsm.br>
2020-04-11 18:05:52 -0300

    add pdfs

--- trab2/gl_1_canvasGlut/src/main.cpp
+++ trab2/gl_1_canvasGlut/src/main.cpp
@@ -11,88 +11,76 @@
 
 #include <GL/glut.h>
 #include <GL/freeglut_ext.h> //callback da wheel do mouse.
-#include <vector>
+#include <list>
+#include <algorithm>
+#include <stdlib.h>
+#include <iostream>
 #include "main.h"
 #include "Botao.h"
-
+#include <type_traits>
 using namespace std;
 
 
 //variavel global para selecao do que sera exibido na canvas.
-int opcao  = 50;
+int tamanho = 30;
 int screenWidth = 1080, screenHeight = 720; //largura e altura inicial da tela . Alteram com o redimensionamento de tela.
 int mouseX, mouseY; //variaveis globais do mouse para poder exibir dentro da render().
-char figuraSelecionada;
-vector<Botao>botoes;
-vector<Circulo>circulos;
-vector<Triangulo>triangulos;
+char proximaFigura;
+char str[100];
+bool preenchida;
+Figura* figuraSelecionada;
+list<Botao>botoesFiguras;
+list<Botao>botoesTamanho;
+list<Figura*>figuras;
 
+
+void iniciaBotoes()
+{
+    botoesFiguras.push_back(Botao(940,600,100,40,"Circulo",'c'));
+    botoesFiguras.push_back(Botao(940,550,100,40,"Triangulo",'t'));
+    botoesFiguras.push_back(Botao(940,500,100,40,"Quadrado",'q'));
+    botoesTamanho.push_back(Botao(930,20,50,40,"+",'+'));
+    botoesTamanho.push_back(Botao(1000,20,50,40,"-",'-'));
+
+}
 void renderizaBotoes()
 {
-    for(auto b = botoes.begin();b!=botoes.end();++b)
+    for(list<Botao>::iterator bf = botoesFiguras.begin();bf!=botoesFiguras.end();++bf)
     {
-        switch(b->getFigura())
+        switch(bf->getFigura())
         {
             case 'c':
-                b->Render(0,0,255,255,255,255);
+                bf->Render(0,0,255,255,255,255);
                 break;
             case 't':
-                b->Render(255,0,0,255,255,255);
+                bf->Render(255,0,0,255,255,255);
                 break;
+            case 'q':
+                bf->Render(0,255,0,0,0,0);
         }
-
+    }
+    for(list<Botao>::iterator bt = botoesTamanho.begin();bt!=botoesTamanho.end();++bt)
+    {
+        bt->Render(255,255,0,0,0,0);
     }
 }
-void DrawMouseScreenCoords()
-{
-    char str[100];
-    sprintf(str, "Mouse: (%d,%d)", mouseX, mouseY);
-    text(10,300, str);
-    sprintf(str, "Screen: (%d,%d)", screenWidth, screenHeight);
-    text(10,320, str);
+void renderizaLabels(){
+    color(1,1,1);
+    sprintf(str, "TAMANHO: (%d)",tamanho);
+    text(920,70, str);
 }
-
 //funcao chamada continuamente. Deve-se controlar o que desenhar por meio de variaveis globais
 //Todos os comandos para desenho na canvas devem ser chamados dentro da render().
 //Deve-se manter essa função com poucas linhas de codigo.
 void render()
 {
-   DrawMouseScreenCoords(mouseX,mouseY,screenWidth,screenHeight);
    desenhaGUI(screenWidth,screenHeight);
    renderizaBotoes();
-
-   for(auto c = circulos.begin();c!=circulos.end();++c)
+   renderizaLabels();
+   for(list<Figura*>::iterator f = figuras.begin();f!=figuras.end();++f)
    {
-       c->Render(0,0,0);
+       (*f)->render();
    }
-   for(auto t = triangulos.begin();t!=triangulos.end();++t)
-   {
-       t->Render(0,0,0);
-   }
-}
-
-//funcao chamada toda vez que uma tecla for pressionada.
-void keyboard(int key)
-{
-   printf("\nTecla: %d" , key);
-   if( key < 200 )
-   {
-      opcao = key;
-   }
-
-   switch(key)
-   {
-      case 27:
-	     exit(0);
-	  break;
-
-   }
-}
-
-//funcao chamada toda vez que uma tecla for liberada
-void keyboardUp(int key)
-{
-   printf("\nLiberou: %d" , key);
 }
 
 //funcao para tratamento de mouse: cliques, movimentos e arrastos
@@ -103,40 +91,107 @@ void mouse(int button, int state, int wheel, int direction, int x, int y)
 
    printf("\nmouse %d %d %d %d %d %d", button, state, wheel, direction,  x, y);
 
-   if( state == 0 ) //clicou
+   if( button==0 && state == 0 ) //clicou com botao esquerdo
    {
-       for(auto i = botoes.begin();i!=botoes.end();++i)
+       for(list<Botao>::iterator i = botoesFiguras.begin();i!=botoesFiguras.end();++i)
        {
            if( i->Colidiu(x, y) )
            {
-               figuraSelecionada = i->getFigura();
-               printf("\nClicou no %c\n", i->getFigura());
+               proximaFigura = i->getFigura();
            }
 
+       }
+       for(list<Botao>::iterator i = botoesTamanho.begin();i!=botoesTamanho.end();++i)
+       {
+           if(i->Colidiu(x,y))
+           {
+               switch (i->getFigura())
+               {
+                    case '+':
+                        figuraSelecionada->setTamanho(figuraSelecionada->getTamanho()+5);
+                        tamanho+=5;
+                        break;
+                    case '-':
+                        figuraSelecionada->setTamanho(figuraSelecionada->getTamanho()-5);
+                        tamanho-=5;
+                        break;
+               }
+           }
        }
        if(mouseX<880)
        {
-           switch(figuraSelecionada)
-           {
+
+            for(list<Figura*>::iterator i = figuras.begin();i!=figuras.end();++i)
+            {
+                if((*i)->Colidiu(x,y))
+                {
+                    figuraSelecionada = (*i);
+                    cout << endl << "tamanho: "<< figuraSelecionada->getTamanho() << endl;
+                }
+            }
+
+            switch(proximaFigura)
+            {
                 case 'c':
-                    circulos.push_back(Circulo(mouseX,mouseY,30));
+                    figuras.push_back(new Circulo(mouseX,mouseY,tamanho,0,0,0,true));
+                    proximaFigura = NULL;
                     break;
                 case 't':
-                    triangulos.push_back(Triangulo(mouseX,mouseY,30));
+                    figuras.push_back(new Triangulo(mouseX,mouseY,tamanho,0,0,0,true));
+                    proximaFigura = NULL;
                     break;
+                case 'q':
+                    figuras.push_back(new Quadrado(mouseX,mouseY,tamanho,0,0,0,true));
+                    proximaFigura = NULL;
+                    break;
+            }
+        }
+    }
+    else if(button==2 && state==0) //clicou com botao direito
+    {
+       for(list<Figura*>::iterator i = figuras.begin();i!=figuras.end();++i)
+       {
+           if((*i)->Colidiu(x,y))
+           {
+                i = figuras.erase(i);
            }
        }
-   }
+    }
+    else if (wheel==0)
+    {
+       switch(direction)
+       {
+        case 1:
+            for(list<Figura*>::iterator i = figuras.begin();i!=figuras.end();++i)
+            {
+                if((*i)->Colidiu(x,y))
+                {
+                    (*i)->setTamanho((*i)->getTamanho()+5);
+                    tamanho+=5;
+                }
+            }
+            break;
+        case -1:
+            for(list<Figura*>::iterator i = figuras.begin();i!=figuras.end();++i)
+            {
+                if((*i)->Colidiu(x,y))
+                {
+                    if((*i)->getTamanho()>5)
+                    {
+                        (*i)->setTamanho((*i)->getTamanho()-5);
+                        tamanho-=5;
+                    }
+                }
+            }
+            break;
+       }
+    }
 }
-void iniciaBotoes()
-{
-    botoes.push_back(Botao(960,600,100,40,"Circulo",'c'));
-    botoes.push_back(Botao(960,550,100,40,"Triangulo",'t'));
-}
+
 
 int main(void)
 {
-   initCanvas(&screenWidth, &screenHeight, "Titulo da Janela: Canvas 2D - Pressione 1, 2, 3");
+   initCanvas(&screenWidth, &screenHeight, "Editor de Figuras Vetoriais - Pedro Rossato");
    iniciaBotoes();
    runCanvas();
 }
--- trab2/gl_1_canvasGlut/src/triangulo.h
+++ trab2/gl_1_canvasGlut/src/triangulo.h
@@ -1,23 +1,33 @@
 #ifndef TRIANGULO_H
 #define TRIANGULO_H
+#include "Figura.h"
 
-
-class Triangulo
+class Triangulo : public Figura
 {
-    int x,y,raio;
+    float vx[3],vy[3];
     public:
-        Triangulo(int _x, int _y, int _raio)
+        Triangulo(int _x, int _y,int _tamanho,int _r, int _g ,int _b, bool _preenchida) : Figura(_x,_y,_tamanho,_r,_g,_b,_preenchida)
         {
-            x = _x;
-            y = _y;
-            raio = _raio;
-        }
-        void Render(int r, int g,int b)
-        {
-            color((float)r/255,(float)g/255,(float)b/255);
-            circleFill(x,y,raio,3);
-        }
 
+        }
+        void render()
+        {
+            vx[0] = x-tamanho; vx[1] = x; vx[2] = x+tamanho;
+            vy[0] = y-tamanho; vy[1] = y+tamanho; vy[2] = y-tamanho;
+            color((float)r/255,(float)g/255,(float)b/255);
+            if (preenchida)
+            {
+                polygonFill(vx,vy,3);
+            }
+            else
+            {
+                polygon(vx,vy,3);
+            }
+        }
+        bool Colidiu(int x, int y)
+        {
+            return false;
+        }
     protected:
 
     private:
